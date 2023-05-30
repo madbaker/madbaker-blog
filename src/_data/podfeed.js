bfb7c1e86c4824772ced793cbb41a77e1604dfab
Mark Dyck <mark@madbaker.com>
2023-05-29 19:44:53 -0700

    add podcast pages based on rss feed

---
+++ src/_data/podfeed.js
@@ -0,0 +1,30 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+const {XMLParser} = require('fast-xml-parser');
+const { forEach } = require("lodash");
+
+module.exports = async function() {
+
+// get the feed: https://riseuppod.com/rss/?include-libsyn-metadata=true
+
+let xml = await EleventyFetch("https://riseuppod.com/rss/?include-libsyn-metadata=true", {
+    type: "text" // also supports 'json' or 'buffer'
+});
+
+// can add extra parsing options here:
+
+const options = {
+    ignoreAttributes: false,
+    attributeNamePrefix : "@_",
+    transformTagName: (tagName) => tagName.replace(':', '_')
+    //removeNSPrefix: true
+    } ;
+
+// Create a JSON object
+
+const parser = new XMLParser(options);
+
+
+let json = parser.parse(xml);
+
+return json;
+};
