495faa489444da233ef80be80bd9c22263dafd73
Mark Dyck <mark@madbaker.com>
2025-07-14 18:21:58 -0700

    rewrite _data/site.json as site.js

---
+++ src/_data/site.js
@@ -0,0 +1,22 @@
+export default {
+    name: "Mark Dyck",
+    url: "https://madbaker.com",
+    siteName: "Madbaker dot com",
+    siteDescription: "Personal website of Mark Dyck, a baker who writes",
+    homepageTitle: "Mark Dyck -- a baker who writes",
+    mainSiteNav:  [
+      { url: "/", text: "Home" },
+      { url: "/about/", text: "About" },
+      { url: "/diary/", text: "Nature Diary" },
+      { url: "/writing/", text: "Writing" },
+      { url: "/podcast/", text: "Podcast" },
+      { url: "/tags/", text: "Tags" },
+      { url: "/search/", text: "Search" }
+	  ],
+    footerNav:  [
+      { url: "/", text: "Home" },
+      { url: "/subscribe/", text: "Subscribe" },
+      { url: "/contact/", text: "Contact" },
+      { url: "/colophon/", text: "Colophon" }
+	  ]
+  }
--- todo.md
+++ todo.md
@@ -2,9 +2,9 @@ Version 2.x Todo List
 
 [x]  Move navigation to a JSON file like 11tybundle.dev
 [x]  Get all pages using default.njk for layout
+[x]  Search page
 []  rewrite _data/site.json as site.js
 []  Condense / reduce site metadata files into _data/site.js
-[]  Search page
 []  Page layout like Miriam (Summary, Hero, etc.)
 []  Expanded YAML for posts
 []  Tagged posts sorted by quantity, with quantity listed
